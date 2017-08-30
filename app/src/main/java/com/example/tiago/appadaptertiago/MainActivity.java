e5a37fbfca10f4af44825134a555076214a6626b
Tiago Rodrigues <tiago.laureanor@hotmail.com>
2017-08-30 19:50:33 -0300

    Initial commit

---
+++ app/src/main/java/com/example/tiago/appadaptertiago/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.tiago.appadaptertiago;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
